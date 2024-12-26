30ad3b47c1a40aa3850b4466a842792169a09b42
Benno Richters <b.richters@gmail.com>
2024-12-26 11:14:16 +0100

    use tbl_deep_extend

--- lua/kitt/send_request.lua
+++ lua/kitt/send_request.lua
@@ -1,22 +1,6 @@
 local endpoint = os.getenv("OPENAI_ENDPOINT")
 local key = os.getenv("OPENAI_API_KEY")
 
-local function table_concat(...)
-  local result = {}
-
-  for _, tbl in ipairs({ ... }) do
-    for k, v in pairs(tbl) do
-      if type(k) ~= "number" then
-        result[k] = v
-      else
-        table.insert(result, v)
-      end
-    end
-  end
-
-  return result
-end
-
 return function(post)
   return function(body_content, extra_opts)
     local opts = {
@@ -28,7 +12,7 @@ return function(post)
     }
 
     if extra_opts then
-      opts = table_concat(opts, extra_opts)
+      opts = vim.tbl_deep_extend("error", opts, extra_opts)
     end
 
     return post(endpoint, opts)
