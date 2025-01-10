064e5d30b683ca2a9f7c7285c4e0d1dc0faa4550
Benno Richters <b.richters@gmail.com>
2025-01-10 14:56:44 +0100

    experimenting with assistants api

--- lua/kitt/assistant.lua
+++ lua/kitt/assistant.lua
@@ -27,15 +27,91 @@ local function start()
     },
   }
 
-  print(vim.inspect(opts))
   local response = curl.post(end_point, opts)
   local response_body = vim.fn.json_decode(response.body)
-  print(response.body)
   return response_body.id
 end
 
-function create_thread()
+local function create_thread()
+  local end_point = end_point_start .. "threads" .. end_point_end
+
+  local opts = {
+    body = "",
+    headers = {
+      content_type = "application/json",
+      api_key = api_key,
+    },
+  }
+
+  local response = curl.post(end_point, opts)
+  local response_body = vim.fn.json_decode(response.body)
+  return response_body.id
+end
+
+local function add_to_thread(thread_id)
+  local end_point = end_point_start .. "threads/" .. thread_id .. "/messages" .. end_point_end
+
+  local body_content_table = {
+    role = "user",
+    content = "I need to solve the equation `3x + 11 = 14`. Can you help me?"
+  }
+  local body_content = vim.fn.json_encode(body_content_table)
+
+  local opts = {
+    body = body_content,
+    headers = {
+      content_type = "application/json",
+      api_key = api_key,
+    },
+  }
+
+  local response = curl.post(end_point, opts)
+  local response_body = vim.fn.json_decode(response.body)
+  print(vim.inspect(response_body))
+end
+
+local function run_thread(assistant_id, thread_id)
+  local end_point = end_point_start .. "threads/" .. thread_id .. "/runs" .. end_point_end
+
+  local body_content_table = {
+    assistant_id = assistant_id
+  }
+  local body_content = vim.fn.json_encode(body_content_table)
+
+  local opts = {
+    body = body_content,
+    headers = {
+      content_type = "application/json",
+      api_key = api_key,
+    },
+  }
+
+  local response = curl.post(end_point, opts)
+  local response_body = vim.fn.json_decode(response.body)
+  print(vim.inspect(response_body))
+end
+
+local function result(thread_id)
+  local end_point = end_point_start .. "threads/" .. thread_id .. "/messages" .. end_point_end
+
+  local opts = {
+    body = "",
+    headers = {
+      content_type = "application/json",
+      api_key = api_key,
+    },
+  }
+
+  local response = curl.post(end_point, opts)
+  local response_body = vim.fn.json_decode(response.body)
+  print(vim.inspect(response_body))
 end
 
 local assistant_id = start()
-print(assistant_id)
+local thread_id = create_thread()
+add_to_thread(thread_id)
+print("1------------")
+run_thread(assistant_id, thread_id)
+print("2------------")
+result(thread_id)
+print("end------------")
