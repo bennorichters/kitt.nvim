d44bedd562f2f9dc71280239baec6dee09e71293
Benno Richters <b.richters@gmail.com>
2025-01-10 14:36:58 +0100

    experimenting with assistants api

--- lua/kitt/assistant.lua
+++ lua/kitt/assistant.lua
@@ -1,21 +1,21 @@
 local curl = require("plenary.curl")
 
 local resource_name = os.getenv("OPENAI_RESOURCE_NAME")
--- local deployment_name = os.getenv("OPENAI_DEPLOYMENT_NAME")
+local model = os.getenv("OPENAI_MODEL")
 local api_version = os.getenv("OPENAI_API_VERSION")
 local api_key = os.getenv("OPENAI_API_KEY")
 
+local end_point_start = "https://" .. resource_name .. ".openai.azure.com/openai/"
+local end_point_end = "?api-version=" .. api_version
+
 local function start()
-  local end_point = "https://" ..
-      resource_name ..
-      ".openai.azure.com/openai/assistants?api-version=" ..
-      api_version
+  local end_point = end_point_start .. "assistants" .. end_point_end
 
   local body_content_table = {
     instructions = "You are an AI assistant that can write code to help answer math questions.",
     name = "Math Assist",
     tools = { { type = "code_interpreter" } },
-    model = "gpt-4o"
+    model = model
   }
   local body_content = vim.fn.json_encode(body_content_table)
 
@@ -27,9 +27,14 @@ local function start()
     },
   }
 
+  print(vim.inspect(opts))
   local response = curl.post(end_point, opts)
   local response_body = vim.fn.json_decode(response.body)
+  print(response.body)
   return response_body.id
+end
+
+function create_thread()
 end
 
 local assistant_id = start()
