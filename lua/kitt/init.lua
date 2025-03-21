e5fb05546d510063447ac8ce1b2f967c1c1d9502
Benno Richters <b.richters@gmail.com>
2025-03-21 16:46:25 +0100

    moved sending template to separate file

--- lua/kitt/init.lua
+++ lua/kitt/init.lua
@@ -1,8 +1,6 @@
 local buffer_helper = require("kitt.buffer_helper")
-local text_prompt = require("kitt.text_prompt")
-local stream_handler = require("kitt.stream")
-local response_writer = require("kitt.response_writer")
 local send_request_factory = require("kitt.send_request")
+local template_sender_factory = require("kitt.template_sender")
 
 local tpl_body_grammar = require("kitt.templates.grammar")
 local tpl_body_interact = require("kitt.templates.interact_with_content")
@@ -17,55 +15,7 @@ local CFG = {
   timeout = 6000
 }
 
-local send_request
-
-local function encode_text(text)
-  local encoded_text = vim.fn.json_encode(text)
-  return string.sub(encoded_text, 2, string.len(encoded_text) - 1)
-end
-
-local function send_plain_request(body_content)
-  local response = send_request(body_content, { timeout = CFG.timeout })
-
-  if (response.status == 200) then
-    local response_body = vim.fn.json_decode(response.body)
-    local content = response_body.choices[1].message.content
-    return content
-  else
-    print(vim.inspect(response))
-  end
-end
-
-local function send_stream_request(body_content)
-  local ui_select = text_prompt.process_buf_text(text_prompt.prompt)
-  local buf = response_writer.ensure_buf_win()
-  local write = function(content) response_writer.write(content, buf) end
-  local process_stream = stream_handler.process_wrap(stream_handler.parse, ui_select, write)
-  local stream = { stream = vim.schedule_wrap(process_stream) }
-
-  send_request(body_content, stream)
-end
-
-local function send_template(template, stream, ...)
-  local subts = {}
-  local count = select("#", ...)
-  for i = 1, count do
-    local text = select(i, ...)
-    table.insert(subts, encode_text(text))
-  end
-
-  if stream then
-    template.stream = true
-  end
-
-  local body_content = string.format(vim.fn.json_encode(template), unpack(subts))
-
-  if stream then
-    return send_stream_request(body_content)
-  else
-    return send_plain_request(body_content)
-  end
-end
+local template_sender
 
 local M = {}
 
@@ -86,28 +36,29 @@ M.setup = function(user_cfg)
   local endpoint = os.getenv("OPENAI_ENDPOINT")
   local key = os.getenv("OPENAI_API_KEY")
 
-  send_request = send_request_factory(post, endpoint, key)
+  local send_request = send_request_factory(post, endpoint, key)
+  template_sender = template_sender_factory(send_request, CFG.timeout)
 end
 
 M.ai_improve_grammar = function()
-  send_template(tpl_body_grammar, true, buffer_helper.current_line())
+  template_sender(tpl_body_grammar, true, buffer_helper.current_line())
 end
 
 M.ai_set_spelllang = function()
-  local content = send_template(tpl_body_recognize_language, false, buffer_helper.current_line())
+  local content = template_sender(tpl_body_recognize_language, false, buffer_helper.current_line())
   if (content) then
     vim.cmd("set spelllang=" .. content)
   end
 end
 
 M.ai_write_minutes = function()
-  send_template(tpl_body_minutes, true, buffer_helper.visual_selection())
+  template_sender(tpl_body_minutes, true, buffer_helper.visual_selection())
 end
 
 M.ai_interactive = function()
   vim.ui.input({ prompt = "Give instructions" }, function(command)
     if command then
-      send_template(tpl_body_interact, true, command, buffer_helper.visual_selection())
+      template_sender(tpl_body_interact, true, command, buffer_helper.visual_selection())
     end
   end)
 end
--- lua/kitt/template_sender.lua
+++ lua/kitt/template_sender.lua
@@ -1,62 +1,53 @@
-local send
+local text_prompt = require("kitt.text_prompt")
+local stream_handler = require("kitt.stream")
+local response_writer = require("kitt.response_writer")
 
 local function encode_text(text)
   local encoded_text = vim.fn.json_encode(text)
   return string.sub(encoded_text, 2, string.len(encoded_text) - 1)
 end
 
-local ts = {}
+return function(send_request, timeout)
+  local send_plain_request = function(body_content)
+    local response = send_request(body_content, { timeout = timeout })
 
-ts.init = function(send_request)
-  send = send_request
-end
-
-local function send_plain_request(body_content)
-  local response = send_request(body_content, { timeout = CFG.timeout })
-
-  if (response.status == 200) then
-    local response_body = vim.fn.json_decode(response.body)
-    local content = response_body.choices[1].message.content
-    return content
-  else
-    print(vim.inspect(response))
-  end
-end
-
-local function send_stream_request(body_content)
-  local select = text_prompt.process_buf_text(text_prompt.prompt)
-  local buf = response_writer.ensure_buf_win()
-  local process_stream = stream_handler.process_wrap(
-    stream_handler.parse, select, response_writer.write, buf
-  )
-
-  local stream = { stream = vim.schedule_wrap(process_stream) }
-
-  send_request(body_content, stream)
-end
-
-ts.send_template = function(template, stream, ...)
-  local subts = {}
-  local count = select("#", ...)
-  for i = 1, count do
-    local text = select(i, ...)
-    table.insert(subts, encode_text(text))
+    if (response.status == 200) then
+      local response_body = vim.fn.json_decode(response.body)
+      local content = response_body.choices[1].message.content
+      return content
+    else
+      print(vim.inspect(response))
+    end
   end
 
-  if stream then
-    template.stream = true
+  local send_stream_request = function(body_content)
+    local ui_select = text_prompt.process_buf_text(text_prompt.prompt)
+    local buf = response_writer.ensure_buf_win()
+    local write = function(content) response_writer.write(content, buf) end
+    local process_stream = stream_handler.process_wrap(stream_handler.parse, ui_select, write)
+    local stream = { stream = vim.schedule_wrap(process_stream) }
+
+    send_request(body_content, stream)
   end
 
-  local body_content = string.format(vim.fn.json_encode(template), unpack(subts))
+  return function(template, stream, ...)
+    local subts = {}
+    local count = select("#", ...)
+    for i = 1, count do
+      local text = select(i, ...)
+      table.insert(subts, encode_text(text))
+    end
 
-  if stream then
-    return send_stream_request(body_content)
-  else
-    return send_plain_request(body_content)
+    if stream then
+      template.stream = true
+    end
+
+    local body_content = string.format(vim.fn.json_encode(template), unpack(subts))
+
+    if stream then
+      return send_stream_request(body_content)
+    else
+      return send_plain_request(body_content)
+    end
   end
 end
-
-local M = {}
-
-
-return ts
