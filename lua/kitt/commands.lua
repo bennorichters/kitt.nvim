277772cef9ecd94cd041b0e93ae00baa38d03064
Benno Richters <b.richters@gmail.com>
2025-03-22 20:10:14 +0100

    moved commands into separate testable file

---
+++ lua/kitt/commands.lua
@@ -0,0 +1,36 @@
+local tpl_grammar = require("kitt.templates.grammar")
+local tpl_interact = require("kitt.templates.interact_with_content")
+local tpl_minutes = require("kitt.templates.minutes")
+local tpl_recognize_language = require("kitt.templates.recognize_language")
+
+local M = {}
+
+M.setup = function(buffer_helper, template_sender)
+  M.buffer_helper = buffer_helper
+  M.template_sender = template_sender
+end
+
+M.ai_improve_grammar = function()
+  M.template_sender(tpl_grammar, true, M.buffer_helper.current_line())
+end
+
+M.ai_set_spelllang = function()
+  local content = M.template_sender(tpl_recognize_language, false, M.buffer_helper.current_line())
+  if (content) then
+    vim.cmd("set spelllang=" .. content)
+  end
+end
+
+M.ai_write_minutes = function()
+  M.template_sender(tpl_minutes, true, M.buffer_helper.visual_selection())
+end
+
+M.ai_interactive = function()
+  vim.ui.input({ prompt = "Give instructions" }, function(command)
+    if command then
+      M.template_sender(tpl_interact, true, command, M.buffer_helper.visual_selection())
+    end
+  end)
+end
+
+return M
--- lua/kitt/init.lua
+++ lua/kitt/init.lua
@@ -2,11 +2,7 @@ local config = require("kitt.config")
 local buffer_helper = require("kitt.buffer_helper")
 local send_request_factory = require("kitt.send_request")
 local template_sender_factory = require("kitt.template_sender")
-
-local tpl_body_grammar = require("kitt.templates.grammar")
-local tpl_body_interact = require("kitt.templates.interact_with_content")
-local tpl_body_minutes = require("kitt.templates.minutes")
-local tpl_body_recognize_language = require("kitt.templates.recognize_language")
+local commands = require("kitt.commands")
 
 local log = require("kitt.log")
 log.trace("kitt log here")
@@ -34,29 +30,24 @@ M.setup = function(user_cfg)
 
   local send_request = send_request_factory(post, endpoint, key)
   template_sender = template_sender_factory(send_request, config.get().timeout)
+
+  commands.setup(buffer_helper, template_sender)
 end
 
 M.ai_improve_grammar = function()
-  template_sender(tpl_body_grammar, true, buffer_helper.current_line())
+  commands.ai_improve_grammar()
 end
 
 M.ai_set_spelllang = function()
-  local content = template_sender(tpl_body_recognize_language, false, buffer_helper.current_line())
-  if (content) then
-    vim.cmd("set spelllang=" .. content)
-  end
+  commands.ai_set_spelllang()
 end
 
 M.ai_write_minutes = function()
-  template_sender(tpl_body_minutes, true, buffer_helper.visual_selection())
+  commands.ai_write_minutes()
 end
 
 M.ai_interactive = function()
-  vim.ui.input({ prompt = "Give instructions" }, function(command)
-    if command then
-      template_sender(tpl_body_interact, true, command, buffer_helper.visual_selection())
-    end
-  end)
+  commands.ai_interactive()
 end
 
 return M
